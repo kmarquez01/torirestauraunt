94c0a3c2584397361659e81c1569e1f2ad889e67
kmarquez <kmarquez@sfu.ca>
2024-01-23 21:02:52 -0800

    cart functionality

--- contexts/shop-context.js
+++ contexts/shop-context.js
@@ -1,6 +1,5 @@
 import React, {createContext, useState} from 'react'
 import CardInfo from '../Menu/CardInfo'
-import counter from '../Menu/CardItem'
 
 
 export const ShopContext = createContext(null)
