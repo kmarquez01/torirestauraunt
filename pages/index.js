e8e4fbe73d1a915b3b34033fc9e2e4ea22bc126a
kmarquez <kmarquez@sfu.ca>
2024-01-23 20:42:10 -0800

    cart functionality fix attempt 1

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,9 @@ function App(){
       
       <div>
       
-      {/* <ShopContextProvider> */}
+      <ShopContextProvider>
       <Homes />
-      {/* </ShopContextProvider> */}
+      </ShopContextProvider>
       </div>
                 
    
